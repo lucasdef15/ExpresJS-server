b37853556f0adb20573461bd2b7dbc64f05e4166
lucasdef15 <lucasff15@hotmail.com>
2025-04-29 19:46:09 -0300

    new file:   .gitignore 	new file:   README.md 	new file:   config/corsOptions.js 	new file:   controllers/employeesController.js 	new file:   logs/errLog.txt 	new file:   logs/reqLog.txt 	new file:   middleware/errorHandler.js 	new file:   middleware/logEvents.js 	new file:   model/employees.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/css/style.css 	new file:   public/img/img1.jpg 	new file:   public/text/data.txt 	new file:   routes/api/employees.js 	new file:   routes/root.js 	new file:   routes/subdir.js 	new file:   server.js 	new file:   views/404.html 	new file:   views/index.html 	new file:   views/new-page.html 	new file:   views/subdir/index.html 	new file:   views/subdir/test.html

---
+++ config/corsOptions.js
@@ -0,0 +1,18 @@
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
---
+++ controllers/employeesController.js
@@ -0,0 +1,90 @@
+const data = {
+  employees: require("../model/employees.json"),
+  setEmployees: function (data) {
+    this.employees = data;
+  },
+};
+
+const getAllEmployees = (req, res) => {
+  res.json(data.employees);
+};
+
+const createNewEmployee = (req, res) => {
+  const newEmployee = {
+    id: data.employees.length
+      ? data.employees[data.employees.length - 1].id + 1
+      : 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  };
+
+  if (!newEmployee.firstname || !newEmployee.lastname) {
+    return res
+      .status(400)
+      .json({ message: "Fisrt and last names are required." });
+  }
+
+  data.setEmployees([...data.employees, newEmployee]);
+  res.status(201).json(data.employees);
+};
+
+const updateEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found.` });
+  }
+
+  if (req.body.firstname) employee.firstname = req.body.firstname;
+  if (req.body.lastname) employee.lastname = req.body.lastname;
+
+  // Remove o antigo (com mesmo id)
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+
+  // Atualiza o array, com o novo objeto
+  data.setEmployees([...filteredArray, employee].sort((a, b) => a.id - b.id));
+
+  res.json(data.employees);
+};
+
+const deleteEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
+};
+
+const getEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.params.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.params.id} not found` });
+  }
+  res.json(employee);
+};
+
+module.exports = {
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee,
+};
